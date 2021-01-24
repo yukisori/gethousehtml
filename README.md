8f7b80db0ec04e4537348238e9f122ec395bccda
佐藤佑樹 <satouyuuki@satouyuukinoiMac.local>
2021-01-24 11:47:30 +0900

    first

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # gethousehtml
 
 利用方法
+動かせるよにする
+npm install
+ 
+サーバー立ち上げる
 node app.js
 http://localhost:3000
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const puppeteer = require('puppeteer');
+const app = express();
+
+const server = app.listen(3000, function() {
+	console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res, next) {
+	res.render("index", { link: "", result: "" });
+});
+
+app.post("/comp", function(req, res, next) {
+	let throwLink = req.body.url;
+	(async () => {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto(req.body.url);
+		let resultTxt = await page.$eval('#vue-data', e => e.outerHTML);
+		await browser.close();
+		await res.render("index", {
+			link: throwLink,
+			result: resultTxt,
+		});
+	})();
+});
